2b5299186224edfd880ebe7d6c7d74abd2ebad00
Ilian <ilian.marchitto@etu.univ-amu.fr>
2026-02-02 00:11:46 +0100

    ajouts des différents fichiers

---
+++ includes/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IL FAUT METTRE UN TITRE</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="about.php">À propos</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>    
+</body>
+</html>
